46026b6bfd74f2c958cf1e6603d486f2b6098f57
abutaleb08sae <sardar2207108@stud.kuet.ac.bd>
2025-12-31 11:49:19 +0600

    add gui compomemt

--- src/main/java/com/example/weatherapp/HelloController.java
+++ src/main/java/com/example/weatherapp/HelloController.java
@@ -4,11 +4,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label tempLabel; // This matches the fx:id in FXML
+
+    // This is the method IntelliJ is complaining about!
+    @FXML
+    protected void onButtonClick() {
+        System.out.println("Refresh button clicked!");
+        tempLabel.setText("Updating...");
+
+        // This is where you will later call your API/Database logic
     }
 }
