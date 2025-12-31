09a2b0353c2e1eedf3c90e35bbe0306bc2f49024
abutaleb08sae <sardar2207108@stud.kuet.ac.bd>
2025-12-31 10:45:06 +0600

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.weatherapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.weatherapp to javafx.fxml;
+    exports com.example.weatherapp;
+}
